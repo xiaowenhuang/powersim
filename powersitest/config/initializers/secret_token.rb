de067d4064d53da57768301d634410d1cb35fe23
XiaoWen.Huang <hxw110@gmail.com>
2012-09-04 14:47:08 +0800

    i give up the false ruby,gems and mysql2,and rails version
    i will rebuild the app and first configuration my environment in the
    morning.
    develop with rails under windows os seems troublesome

--- powersitest/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Powersitest::Application.config.secret_token = '70dec7731257eb9e2c47d41ebb276947e5e4ffd56f401d1cb1be80bf00995b479e1eb52097d52a28888a9db3a6416bc417952aecebd40ab79d5c6084d8ed1fe2'
