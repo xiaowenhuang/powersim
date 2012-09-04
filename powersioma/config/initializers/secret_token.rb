59541c08a88156eadac7dc3ccb599f5bc5907eac
XiaoWen.Huang <hxw110@gmail.com>
2012-09-04 14:52:42 +0800

    new powersioma is powersiom's new version
    it is build in ruby1.92 gem1.3.7 rails 3.0.0 mysql2-0.2.6 and local
    mysql5.5
    just like  agile web development with rails fourth

---
+++ powersioma/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Powersioma::Application.config.secret_token = 'e83daa17914d9bb93b23ea09625fc9535d1840319a55e2279476eee46a7e33cc3afbc353cdd822ad56635106ae854043ebcb5d290e07666f09f4ce7ce3fa9faf'
