d351e2c06343e96eb5ccb256dd5b1a815c3845bc
XiaoWen.Huang <hxw110@gmail.com>
2012-09-05 11:39:34 +0800

    ?sdf
    ?sdf

--- powersioma/db/schema.rb
+++ powersioma/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120904071846) do
+ActiveRecord::Schema.define(:version => 20120905012033) do
 
   create_table "pharmacies", :force => true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(:version => 20120904071846) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "operator"
   end
 
 end
