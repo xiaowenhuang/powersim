28c3b7f566bceff9ddc985c836ebe211c2f8a5f3
XiaoWen.Huang <hxw110@gmail.com>
2012-09-11 17:34:59 +0800

    nearly do nothing
    i am busy with company's task so have no time to design my rails'
    development

--- powersioma/app/models/user.rb
+++ powersioma/app/models/user.rb
@@ -5,9 +5,19 @@ class User < ActiveRecord::Base
   attr_accessor            :password_confirmation
   validates_confirmation_of  :hashed_password
   validate                   :password_non_blank
+
+  def create_new_salt
+    self.salt=self.object_id.to_s+ rand.to_s
+  end
+  
  private
    def password_non_blank
      errors.add(:password,"不能为空")if hashed_password.blank?
    end
+   
+   def self.encrypted_password(password,salt)
+    string_to_hash=password+"wibble"+salt
+    Digest::SHA1.hexdigest(string_to_blank)
+  end
   
 end
